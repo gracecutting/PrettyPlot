4689ef29e448f2cebc689952b63dd661367f2e0f
Grace Cutting <gracecutting@graces-air.lan>
2020-09-25 16:26:59 -0400

    Complete Lab:PrettyPlot

--- js/pretty.js
+++ js/pretty.js
@@ -70,24 +70,65 @@ var makeTranslateString = function(x,y)
 var drawAxes = function(graphDim,margins,
                          xScale,yScale)
 {
-   
- 
+    var xAxis = d3.axisBottom(xScale)
+    d3.select("svg")
+      .append("g")
+      .attr("transform","translate("+margins.left+","+(graphDim.height+margins.top)+")")
+      .call(xAxis)
+      
+ var yAxis = d3.axisLeft(yScale)
+    d3.select("svg")
+      .append("g")
+      .attr("transform","translate("+margins.left+","+(margins.top)+")")
+      .call(yAxis)   
+  
 }
 
-
 //graphDim -object that stores dimensions of the graph area
-//margins - objedct that stores the size of the margins
+//margins - object that stores the size of the margins
 var drawLabels = function(graphDim,margins)
 {
+   var labels = d3.select("svg")
+        .append("g")
+        .classed("labels",true)
+        
+   //title 
+   labels.append("text")
+        .text("Trump Support")
+        .classed("title",true)
+        .attr("text-anchor","middle")
+        .attr("x", margins.left+(graphDim.width/2))
+        .attr("y", margins.top)
     
+    //x-ais
+    labels.append("text")
+        .text("Percent White")
+        .classed("label",true)
+        .attr("text-anchor","middle")
+        .attr("x",margins.left+(graphDim.width/2))
+        .attr("y",margins.top+graphDim.height+margins.bottom)
+    
+    //y-axis
+    labels.append("g")
+        .attr("transform","translate(5,"+ 
+              (margins.top+(graphDim.height/2))+")")
+        .append("text")
+        .text("Percentage Voting for Trump")
+        .classed("label",true)
+        .attr("text-anchor","middle")
+        .attr("transform","rotate(90)")
+     
 }
 
 
 var drawLegend = function(graphDim,margins)
 {
+    var legend = d3.select("svg")
+                    .append("g")
+                    .classed("legend", true)
+                    .attr("transform", "translate("+(margins.left)+","+(margins.top)+")");
     
- 
-   var categories = [
+    var categories = [
        {
            class:"lessCollege",
            name:"Less College"
@@ -97,11 +138,39 @@ var drawLegend = function(graphDim,margins)
            name:"High unemployment"
        }
     ]
-
-
+    
+    var entries = legend.selectAll("g")
+        .data(categories)
+        .enter()
+        .append("g")
+        .classed("legendEntry",true)
+        .attr("class", function(category)
+        {
+            return category.class
+        })
+        .attr("transform", function(category,index)
+        {
+           return "translate(0,"+(index*20)+")";
+        })
     
     
+        
+    entries.append("circle")
+            .attr("cx",8)
+            .attr("cy",5)
+            .attr("r", 5)
+            .attr("class", function(stat)
+            {
+                return stat.class
+            })
     
+    entries.append("text")
+            .text(function(category)
+            {
+                return category.name
+            })
+            .attr("x",15)
+            .attr("y",10)  
 }
 
 //sets up several important variables and calls the functions for the visualization.
@@ -110,7 +179,7 @@ var initGraph = function(counties)
     //size of screen
     var screen = {width:800,height:600}
     //how much space on each side
-    var margins = {left:30,right:20,top:20,bottom:30}
+    var margins = {left:50,right:150,top:40,bottom:40}
     
     
     
@@ -159,6 +228,7 @@ var successFCN = function(counties)
 {
     console.log("politics",counties);
     initGraph(counties);
+    
 }
 
 var failFCN = function(error)
